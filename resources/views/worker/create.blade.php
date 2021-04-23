05fa3cffae5e56926ab6be07b6f27ac11dc5465b
Danilo Patané <d.patane98@gmail.com>
2021-04-23 10:04:17 +0200

    scanning half complete

--- resources/views/worker/create.blade.php
+++ resources/views/worker/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 
-<div class="container">
+<div id="scan-container" class="container">
 
 
     
@@ -42,14 +42,14 @@
     <div class="form-group col-md-6 col-6">
       
       <label for="codice_prodotto">codice prodotto</label>
-      <input name="codice_prodotto" type="number" id="codice_prodotto" class="form-control test">
+      <input name="codice_prodotto" type="number" id="codice_prodotto" class="form-control code-scanner" value="">
       @error('codice_prodotto')
       <div class="alert alert-danger">{{ $message }}</div>
     @enderror
     </div>
     <div class="form-group col-md-6 col-6">
       <label for="codice_stock">codice stock</label>
-      <input name="codice_stock" type="number" id="codice_stock" class="form-control test" >
+      <input name="codice_stock" type="number" id="codice_stock" class="form-control code-scanner" value="">
       @error('codice_stock')
       <div class="alert alert-danger">{{ $message }}</div>
     @enderror
@@ -59,7 +59,7 @@
 
     <div class="form-group col-md-4 col-4">
       <label for="scaffale">scaffale</label>
-      <input name="scaffale" type="text"  id="scaffale" class="form-control">
+      <input name="scaffale" type="text"  id="scaffale" class="form-control" value="">
       @error('scaffale')
       <div class="alert alert-danger">{{ $message }}</div>
     @enderror
@@ -67,14 +67,14 @@
 
     <div class="form-group col-md-4 col-4">
       <label for="quantita_rimanente">quantita_rimanente</label>
-      <input name="quantita_rimanente" type="number"  id="quantita_rimanente" class="form-control"> 
+      <input name="quantita_rimanente" type="number"  id="quantita_rimanente" class="form-control" value=""> 
       @error('quantita_rimanente')
       <div class="alert alert-danger">{{ $message }}</div>
     @enderror
     </div>
     <div class="form-group col-md-4 col-4">
       <label for="data_di_scadenza">data_di_scadenza</label>
-      <input name="data_di_scadenza" type="date" id="data_di_scadenza" class="form-control" >
+      <input name="data_di_scadenza" type="date" id="data_di_scadenza" class="form-control" value="" >
       @error('data_di_scadenza')
       <div class="alert alert-danger">{{ $message }}</div>
     @enderror
@@ -84,7 +84,7 @@
 
     <div class="form-group col-md-6 col-6">
       <label for="name">name</label>
-      <input name="name" type="text" class="form-control" id="name">
+      <input name="name" type="text" class="form-control" id="name" value="">
       @error('name')
       <div class="alert alert-danger">{{ $message }}</div>
     @enderror
@@ -92,7 +92,7 @@
 
     <div class="form-group col-md-6 col-6">
       <label for="description">description</label>
-      <input name="description" type="text" class="form-control" id="description">
+      <input name="description" type="text" class="form-control" id="description" value="">
       @error('description')
       <div class="alert alert-danger">{{ $message }}</div>
     @enderror
@@ -102,7 +102,7 @@
 
     <div class="form-group col-md-6 col-6">
       <label for="costo">costo</label>
-      <input name="costo" type="number" step=0.01 class="form-control" id="costo">
+      <input name="costo" type="number" step=0.01 class="form-control" id="costo" value="">
       @error('costo')
       <div class="alert alert-danger">{{ $message }}</div>
     @enderror
@@ -110,7 +110,7 @@
     
     <div class="form-group col-md-6 col-6">
       <label for="settore">settore</label>
-      <input name="settore" type="text" class="form-control" id="settore">
+      <input name="settore" type="text" class="form-control" id="settore" value="">
       @error('settore')
       <div class="alert alert-danger">{{ $message }}</div>
     @enderror
@@ -123,12 +123,88 @@
 </div>
 
 <script type="text/javascript">
+  // code scanning function 
+  (function ($) {
+    $.fn.codeScanner = function (options) {
+        var settings = $.extend({}, $.fn.codeScanner.defaults, options);
 
+        return this.each(function () {
+            var pressed = false;
+            var chars = [];
+            var $input = $(this);
+
+            $(window).keypress(function (e) {
+                var keycode = (e.which) ? e.which : e.keyCode;
+                if ((keycode >= 65 && keycode <= 90) ||
+                    (keycode >= 97 && keycode <= 122) ||
+                    (keycode >= 48 && keycode <= 57)
+                ) {
+                    chars.push(String.fromCharCode(e.which));
+                }
+                // console.log(e.which + ":" + chars.join("|"));
+                if (pressed == false) {
+                    setTimeout(function () {
+                        if (chars.length >= settings.minEntryChars) {
+                            var barcode = chars.join('');
+                            settings.onScan($input, barcode);
+                        }
+                        chars = [];
+                        pressed = false;
+                    }, settings.maxEntryTime);
+                }
+                pressed = true;
+            });
+
+            $(this).keypress(function (e) {
+                if (e.which === 13) {
+                    e.preventDefault();
+                }
+            });
+
+            return $(this);
+        });
+    };
+
+    $.fn.codeScanner.defaults = {
+        minEntryChars: 8,
+        maxEntryTime: 100,
+        onScan: function ($element, barcode) {
+            $element.val(barcode);
+        }
+    };
+})(jQuery);
+
+
+  // code scanning function 
+
+
+  // TAKING SCAN 
+   $('#scan-container').codeScanner({
+    maxEntryTime: 100, // milliseconds
+    minEntryChars: 7  // characters
+});
+$('#scan-container').codeScanner({
+    onScan: function ($element, code) {
+        var data_di_scadenza = code.substring(0, 5)
+        $('#data_di_scadenza').val(data_di_scadenza);
+    }
+});
+
+
+  // TAKING SCAN 
+
+  // PREVENT SENT FROM CODE SCANNING
   $("form").keydown(function (e) {
        if (e.keyCode == 13) {
            e.preventDefault();
            return false;
        }
    });
+
+
+
+  // PREVENT SENT FROM CODE SCANNING
+
+
  </script>
 @endsection
